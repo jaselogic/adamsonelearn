2b8b7c35592003bb409fd69f420a4429e15fc363
Justin Dane Vallar <justin.dane.vallar@adamson.edu.ph>
2013-12-25 19:53:39 +0800

    Set proper visibility attributes upon login button click

--- src/com/jaselogic/adamsonelearn/Main.java
+++ src/com/jaselogic/adamsonelearn/Main.java
@@ -1,15 +1,53 @@
 package com.jaselogic.adamsonelearn;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.Document;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class Main extends Activity {
 
+	private Button btnLogin;
+	private EditText txtStudNo;
+	private EditText txtPassword;
+	private ProgressBar pb1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//get views
+		btnLogin = (Button) findViewById(R.id.btn_login);
+		txtStudNo = (EditText) findViewById(R.id.txt_studno);
+		txtPassword = (EditText) findViewById(R.id.txt_password);
+		pb1 = (ProgressBar) findViewById(R.id.progressBar1);
+		
+		btnLogin.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				btnLogin.setVisibility(View.INVISIBLE);
+				txtStudNo.setVisibility(View.INVISIBLE);
+				txtPassword.setVisibility(View.INVISIBLE);
+				
+				pb1.setVisibility(View.VISIBLE);
+			}
+		});
 	}
 
 	@Override
@@ -19,4 +57,34 @@ public class Main extends Activity {
 		return true;
 	}
 
+	private class DownloadDocumentTask extends AsyncTask<String, Void, Document> {
+
+		@Override
+		protected Document doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+	        Document doc = null;
+	        Response res = null;
+	        try {                                              
+	            res = Jsoup.connect("http://learn.adamson.edu.ph/V4/")
+	            		.data("TXTusername", "201013888", "TXTpassword", "288785", "BTNlogin", "Login")
+	            		.method(Method.POST)
+	            		.execute();
+	            Map<String, String> loginCookies = res.cookies();
+	            
+	            doc = Jsoup.connect("http://learn.adamson.edu.ph/V4/?page=welcome")
+	            		.cookies(loginCookies)
+	            		.get();
+	        } catch (IOException e) {                          
+	            e.printStackTrace();                           
+	        }
+	        
+	        return doc;
+		}
+		
+		protected void onPostExecute(Document result) {
+
+		}
+		
+	}
+	
 }
