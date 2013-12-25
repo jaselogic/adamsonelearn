8335415aa94f4bf52dc5fd2462581a25e511f3c2
Justin Dane Vallar <justin.dane.vallar@adamson.edu.ph>
2013-12-25 21:38:37 +0800

    Receive outer html from a:main to a:access in access.java

--- src/com/jaselogic/adamsonelearn/Access.java
+++ src/com/jaselogic/adamsonelearn/Access.java
@@ -2,12 +2,18 @@ package com.jaselogic.adamsonelearn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Access extends Activity {
+	private TextView tv1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_access);
+		
+		tv1 = (TextView) findViewById(R.id.textView1);
+		tv1.setText(getIntent().getExtras().getString("outerHTML"));
 	}
 }
